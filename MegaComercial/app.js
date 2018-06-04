e2a170497fb7e6f6cce81c02d5019db941cabf81
Enrique Aguilar <enriqueao96@gmail.com>
2018-06-03 21:27:58 -0500

    nuevo scraping para superama
    - implementación de puppeter

--- MegaComercial/app.js
+++ MegaComercial/app.js
@@ -1,18 +1,50 @@
 const puppeteer = require('puppeteer');
-
+var mysql = require('mysql');
+var products = [];
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    await page.goto('https://www.lacomer.com.mx/lacomer/goBusqueda.action?succId=376&ver=mislistas&succFmt=100&criterio=7501001604325#/7501001604325');
-
+    await page.goto('https://www.superama.com.mx/catalogo/d-lacteos-y-huevo/f-leche/l-leche');
+    await page.click('.btnSuperama.btnSuperama-blanco');
+    await page.waitFor(15000);
+    await page.click('.btnSuperama.btnSuperama-blanco');
+    await page.waitFor(15000);
+    // await page.click('.btnSuperama.btnSuperama-blanco');
+    // await page.waitFor(15000);
     const textContent = await page.evaluate(() => {
-        return {
-            precioNormal: document.querySelector('.precio_normal').textContent,
-        };
+        let data = []; // Create an empty array that will store our data
+        let elements = document.querySelectorAll(".item.thumb.graphic.portfolio-grid.isotope-item");// Select all Products
+        var id = 121; //el ultimo id + 1
+        for(var element of elements) { // Loop through each proudct
+            let price = element.childNodes[9].innerText.replace("$", "").trim(); // Select the title
+            let title = element.childNodes[7].innerText.trim(); // Select the price
+            let img = element.childNodes[1].childNodes[0].childNodes[0].src; // Select the price
+            let upc = element.childNodes[1].childNodes[0].childNodes[1].getAttribute('data-upc'); // Select the price
+            
+            if (title != ""){
+                data.push({ id, title, price, img, upc }); // Push an object with the data onto our array
+                id++;
+            }
+        }   
+
+        return data; // Return our data array
     });
-
-    console.log(textContent); /* No Problem Mate */
-
+    //console.log(textContent); /* No Problem Mate */
     browser.close();
+    querys(textContent);
 })();
+
+
+var fs = require('fs');
+function querys(datos){
+    var txtFile = fs.createWriteStream("querys.txt");
+    txtFile.once('open', function (fd) {
+        for (var element of datos) {
+            txtFile.write('INSERT INTO products VALUES(' + element.id + ',NULL ,"' + element.title + '","' + element.upc + '","' + element.img + '",NOW(),NOW()); \n');
+            txtFile.write('INSERT INTO productsprices VALUES(NULL ,' + element.id + ',"' + element.price + '",4 ,10 ,NOW(),NOW()); \n');
+        }
+        txtFile.end();
+        console.log("Listo!");
+    });
+}
---
+++ MegaComercial/checkDigit.js
@@ -0,0 +1,45 @@
+upc = "0097339000054";
+// console.log(upc.length);
+for (var i = 0; i < upc.length; i++) {
+    if (upc.charAt(i) != '0') {
+        upc_format = upc.substring(i);
+        break;
+    }
+}
+// console.log(upc_format);
+if (upc_format.substring(0,3) == "750") {
+  if (upc_format.length == 12) {
+
+    var sumar = 0;
+    var sumar2 = 0;
+    for (var i = 0; i < upc_format.length; i=i+2 ) {
+
+          nones = upc_format[i];
+          sumar = parseInt(nones) + sumar;
+          // console.log(nones);
+    }
+    // console.log("sumar: "+sumar);
+    for (var j =1; j < upc_format.length; j=j+2) {
+      pares = upc_format[j];
+      multi = pares*3;
+      sumar2 = parseInt(multi) + sumar2;
+      // console.log(pares);
+    }
+    // console.log("sumar2: "+sumar2);
+
+    total = sumar+sumar2;
+    round = (total+10)-(total%10);
+    digit = round-total;
+    // console.log(digit);
+    if (digit == 0) {
+      console.log(upc)
+    }else{
+      console.log(upc_format+digit);
+    }
+  }
+  else{
+    console.log(upc_format);
+  }
+}else{
+  console.log(upc_format);
+}
